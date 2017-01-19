7a5a3d8838adc78faade1d0ae2e6f1cf1d6c26ee
Sergio <yarunin1995@mail.ru>
2017-01-20 00:10:16 +0300

    Доработал страницу о нас

--- js.js
+++ js.js
@@ -1,23 +1,14 @@
-/*$(document).ready(init);
-function init() {	
-		$("#elem").click(
-     function(){
-	$('h1').text('changed Value');
-	 $('h1').css('color','red');
-	 
-	 }
-  );
-  }*/
+
   $(document).ready(contacts);
   function contacts() {	
-		$(".contacts").click(
+		$(".contacts").click( //клик
      function(){
 	$('h1').text('CONTACTS'); 
 	$('section').html('<h2>Новый заголовок</h2><p>Новый текст, который содержит <a href="#">другую ссылку</a>.</p>');
 	$('.contacts').css('background','#29c5e6');
 	$('.contacts a').css('color','#FFF');
 	 }
-  );
+	 );
   }
   $(function() {
  
